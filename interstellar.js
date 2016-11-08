46659e5ef2c3d46f083744cbac846b8488184330
Brian <bmansfieldhfl@gmail.com>
2016-11-08 13:57:13 -0500

    Cleaned up interstellar.js, moved note dictionary to it's own file, added one sample button on page

--- interstellar.js
+++ interstellar.js
@@ -1,124 +1,27 @@
-var frequencyDict = [
-    ['C1', 32.703],
-    ['C2', 65.406],
-    ['C3', 130.813],
-    ['C4', 261.626],
-    ['C5', 523.251],
-    ['C6', 1046.502],
+/*  interstellar.js
+    Author: Brian Mansfield
 
-    ['C#1', 34.648],
-    ['C#2', 69.296],
-    ['C#3', 138.591],
-    ['C#4', 277.183],
-    ['C#5', 554.365],
-    ['C#6', 1108.731],
+    This file provides functions to create WAV audio files in javascript.
+    The recommeded entry point for creating a file given a note is
+    the function createWaveFromNote().
 
-    ['D1', 36.708],
-    ['D2', 73.416],
-    ['D3', 146.832],
-    ['D4', 293.665],
-    ['D5', 587.330],
-    ['D6', 1174.659],
+*/
 
-    ['D#1', 38.891],
-    ['D#2', 77.782],
-    ['D#3', 155.563],
-    ['D#4', 311.127],
-    ['D#5', 622.254],
-    ['D#6', 1244.508],
-
-    ['E1', 41.203],
-    ['E2', 82.407],
-    ['E3', 164.814],
-    ['E4', 329.628],
-    ['E5', 659.255],
-    ['E6', 1318.51],
-
-    ['F1', 43.654],
-    ['F2', 87.307],
-    ['F3', 174.614],
-    ['F4', 349.228],
-    ['F5', 698.456],
-    ['F6', 1396.913],
-
-    ['F#1', 46.249],
-    ['F#2', 92.499],
-    ['F#3', 184.997],
-    ['F#4', 369.994],
-    ['F#5', 739.989],
-    ['F#6', 1479.978],
-
-    ['G1', 48.999],
-    ['G2', 97.999],
-    ['G3', 195.998],
-    ['G4', 391.995],
-    ['G5', 783.991],
-    ['G6', 1567.982],
-
-    ['G#1', 51.913],
-    ['G#2', 103.826],
-    ['G#3', 207.652],
-    ['G#4', 415.305],
-    ['G#5', 830.609],
-    ['G#6', 1661.219],
-
-    ['A1', 55],
-    ['A2', 110],
-    ['A3', 220],
-    ['A4', 440],
-    ['A5', 880],
-    ['A6', 1760],
-
-    ['A#1', 58.270],
-    ['A#2', 116.541],
-    ['A#3', 233.082],
-    ['A#4', 466.164],
-    ['A#5', 932.328],
-    ['A#6', 1864.655],
-
-    ['B1', 61.735],
-    ['B2', 123.471],
-    ['B3', 246.942],
-    ['B4', 493.883],
-    ['B5', 987.767],
-    ['B6', 1975.533],
-];
-
-function playNote(note){
-    var audio = makeNoteFromString(note, 1, "");
+function createWaveFromNote(note){
+    var waveSamples = makeSamplesFromNote(note);
+    var audio = makeAudioFromSamples(waveSamples);
     audio.play();
 }
 
-function playDistortedNote(note){
-    var audio = makeNoteFromString(note, 1, "clip");
-    audio.play();
-}
-
-function makeNoteFromString(note, time, filter){
-    var found = false;
-    var audio;
-    for (var i = 0; i < frequencyDict.length; i++){
-        if (frequencyDict[i][0] == note){
-            found = true;
-            var sample;
-            if (filter == 'clip'){
-                sample = makeClippedNoteSample(frequencyDict[i][1], time);
-            } else {
-                sample = makeNoteSample(frequencyDict[i][1], time);
-            }
-
-            audio = makeAudio(sample);
-        }
-    }
-    if (found){
-        return audio;
-    }
+function makeSamplesFromNote(note){
+    var samples = makeNoteSample(frequencyEnum[note], 1);
+    return samples;
 }
 
 function makeNoteSample(frequency, time){
-    var samples = []
+    var samples = [];
     var sample_time = 14400 * time;
-    for (var i = 0; i < sample_time ; i++) {
+    for (var i = 0; i < sample_time; i++) {
       var t = i / 14400;
       samples[i] = Math.sin(frequency * 2 * Math.PI * t);
     }
@@ -129,10 +32,11 @@ function makeClippedNoteSample(frequency, time){
     var pos_clip_thresh = 0.50;
     var neg_clip_thresh = -1 * pos_clip_thresh;
 
-    var samples = []
+    var samples = [];
     var sample_time = 14400 * time;
     for (var i = 0; i < sample_time ; i++) {
       var t = i / 14400;
+
       samples[i] = Math.sin(frequency * 2 * Math.PI * t);
       if (samples[i] > pos_clip_thresh){
           samples[i] = pos_clip_thresh;
@@ -145,7 +49,7 @@ function makeClippedNoteSample(frequency, time){
 }
 
 function makeChordSample(frequencyList, time){
-    var samples = []
+    var samples = [];
     var sample_time = 14400 * time;
     for (var i = 0; i < sample_time ; i++) {
       var t = i / 14400;
@@ -157,7 +61,7 @@ function makeChordSample(frequencyList, time){
     return samples;
 }
 
-function makeAudio(samples){
+function makeAudioFromSamples(samples){
     var wave = new RIFFWAVE();
     wave.header.sampleRate = 14400; // standard sample rate
     wave.header.numChannels = 1;
